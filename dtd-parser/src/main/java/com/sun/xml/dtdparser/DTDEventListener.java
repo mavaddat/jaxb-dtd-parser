24549632beda2a9153abaf73bbccfae474e3a732
Mavaddat Javid <mavaddat.javid@aero.bombardier.com>
2023-03-13 15:48:06 -0400

    link to DTDHandlerBase

--- dtd-parser/src/main/java/com/sun/xml/dtdparser/DTDEventListener.java
+++ dtd-parser/src/main/java/com/sun/xml/dtdparser/DTDEventListener.java
@@ -20,9 +20,9 @@ import java.util.EventListener;
  * All DTD parsing events are signaled through delegate methods of this
  * interface. Client applications should either
  * <ol>
- * <li>implement <code>DTDEventListener</code> and <code>@Override</code>
+ * <li>implement <code>DTDEventListener</code> and override
  * methods that pertain to DTD features they need to process, <strong>or</strong></li>
- * <li>extend <code>DTDHandlerBase</code> and override only those methods 
+ * <li>extend {@link DTDHandlerBase} and override only those methods 
  * that pertain to DTD features they need to process.</li>
  * </ol>
  * <p>
